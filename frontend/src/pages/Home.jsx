6a7b5d9402c883119777b7c92bbece758dec9ab3
iperez-dev <ivan.web9450@gmail.com>
2024-07-14 07:07:09 -0400

    refactoring and commenting home and memory pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import MemoryCard from "../components/MemoryCard";
 import { useContext } from "react";
 import { GlobalContext } from "../context/globalContext";
 import Sidebar from "../components/sidebar/Sidebar";
+import SearchBar from "../components/SearchBar";
+import MemoryForm from "../components/MemoryForm";
 
 function Home() {
   const { memories, query } = useContext(GlobalContext);
 
-  //SEARCH
+  // Search. If query, filter memories for query
   const filteredProducts = query
     ? memories.filter((memory) =>
         memory.stack.toLowerCase().includes(query.toLowerCase())
@@ -14,38 +16,49 @@ function Home() {
     : memories;
 
   return (
-    <div className=" body md:h-[100vh] lg:w-[80%] w-[90%] mx-auto flex flex-col-reverse md:flex-row ">
-      {/* left-column */}
-      <div className=" left-column w-[90%] md:w-[60%] flex-auto mx-auto  ">
-        {filteredProducts.map(
-          ({
-            name,
-            stack,
-            features,
-            url,
-            githubUrl,
-            image,
-            _id,
-            createdAt,
-          }) => (
-            <MemoryCard
-              key={_id}
-              name={name}
-              stack={stack}
-              features={features}
-              url={url}
-              githubUrl={githubUrl}
-              image={image}
-              createdAt={createdAt}
-              _id={_id}
-            />
-          )
-        )}
+    <>
+      {/* body */}
+      <div className=" body md:h-[100vh] lg:w-[80%] w-[90%] mx-auto flex flex-col-reverse md:flex-row ">
+        {/* left-column */}
+        <div className=" left-column w-[90%] md:w-[60%] flex-auto mx-auto  ">
+          {/* memory-cards */}
+          {filteredProducts.map(
+            ({
+              name,
+              stack,
+              features,
+              url,
+              githubUrl,
+              image,
+              _id,
+              createdAt,
+            }) => (
+              <MemoryCard
+                key={_id}
+                name={name}
+                stack={stack}
+                features={features}
+                url={url}
+                githubUrl={githubUrl}
+                image={image}
+                createdAt={createdAt}
+                _id={_id}
+              />
+            )
+          )}
+        </div>
+        {/* right-column */}
+        <div className="right-column w-[90%] md:w-[40%] flex mt-10 mx-auto">
+          {/* <Sidebar /> */}
+          <div className="md:fixed md:px-16 w-[100%] md:w-[34%]">
+            <h1>My Brain</h1>
+            <SearchBar />
+            <br />
+            <MemoryForm />
+          </div>
+        </div>
       </div>
-      {/* right-column */}
-      <div className="right-column w-[90%] md:w-[40%] flex mt-10 mx-auto" ><Sidebar/></div>
-      
-    </div>
+    </>
   );
 }
 
--- frontend/src/pages/Memory.jsx
+++ frontend/src/pages/Memory.jsx
@@ -1,77 +1,98 @@
-
+// Dependencies
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import EditBar from "../components/sidebar/EditBar";
 import { formatDistance } from "date-fns";
 import Axios from "axios";
 
 const Memory = () => {
   const [memoryData, setMemoryData] = useState([]);
+
+  // Get id out off URL
   const { id } = useParams();
 
+  // Get request to api endpoint for single memory
   useEffect(() => {
     const handleMemoryList = async () => {
       try {
         const response = await Axios.get(
           `https://my-brain-app-mern-backend.onrender.com/api/memories/${id}`
         );
+
+        // Error handling
+        if (response.status !== 200) {
+          throw new Error("Failed to fetch data");
+        }
+        console.log(response.data);
+
+        // Set response to local state - memoryData
         setMemoryData(response.data);
       } catch (error) {
-        console.error(error);
+        // Error handling
+        console.error("Error fetching data:", error.message);
       }
     };
     handleMemoryList();
   }, [id]);
 
   // Extract video ID from the YouTube URL if available
+  // Get Video URL
   const videoUrl = memoryData.url || "";
+
+  // Extract video ID from URL
   const videoId = videoUrl.split("v=")[1]?.split("&")[0];
+
+  // Creates embedded link from video URL
   const embedUrl = videoId
     ? `https://www.youtube.com/embed/${videoId}?si=piiS521f9WOa51PA`
     : "";
+  console.log(embedUrl);
 
-  console.log(memoryData);
-
+  // Create a Date object
   const createdAtDate = new Date(memoryData.createdAt);
+  // Format the date to show how long ago it was created using date-fns library
   const formattedDate = !isNaN(createdAtDate)
     ? formatDistance(createdAtDate, new Date(), { addSuffix: true })
     : "Invalid date";
 
   return (
-    <div className="flex flex-col md:flex-row h-auto w-[85%] mx-auto my-8">
-      {/* left-column */}
-      <div className="w-12/12 md:w-8/12 h-full bg-white rounded-lg shadow p-8">
-        {embedUrl && (
-          <div className="video-wrapper">
-            <iframe
-              src={embedUrl}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
+    <>
+      {/* body */}
+      <div className="flex flex-col md:flex-row h-auto w-10/12 mx-auto my-8">
+        {/* left-column */}
+        <div className="w-12/12 md:w-8/12 h-full bg-white rounded-lg shadow p-8">
+        {/* embedded URL */}
+          {embedUrl && (
+            <div className="video-wrapper">
+              <iframe
+                src={embedUrl}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+          )}
+          {/* card-info */}
+          <div>
+            <h2>{memoryData.name}</h2>
+            <p>
+              <strong>Keyword: </strong> {memoryData.stack}
+            </p>
+            <p>
+              <strong>Notes: </strong> {memoryData.features}
+            </p>
+            <Link to={memoryData.githubUrl} target="_blank">
+              <strong>Github </strong>
+            </Link>
+            <p>{formattedDate}</p>
+            <div className="card-description"></div>
           </div>
-        )}
-        <div>
-          <h2>{memoryData.name}</h2>
-          <p>
-            <strong>Keyword: </strong> {memoryData.stack}
-          </p>
-          <p>
-            <strong>Notes: </strong> {memoryData.features}
-          </p>
-          <Link to={memoryData.githubUrl} target="_blank">
-            <strong>Github </strong>
-          </Link>
-          <p>{formattedDate}</p>
-          <div className="card-description"></div>
         </div>
+        {/* right-column */}
+        <EditBar />
       </div>
-
-      {/* right-column */}
-      <EditBar />
-    </div>
+    </>
   );
 };
 
